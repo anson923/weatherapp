2f492fdbc45abc38c5081c9be5eeb8ee5a71dd36
AnsonDev <65697201+anson923@users.noreply.github.com>
2022-11-27 16:06:33 -0800

    Added app UI and search function

--- index.js
+++ index.js
@@ -1,23 +1,119 @@
-// API: https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=986bc7f39c0ed5386e3f0b3005bdb60b
-//KEY: &APPID=986bc7f39c0ed5386e3f0b3005bdb60b
-//Search: ?q=
+/*
+WEATHER API: https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=986bc7f39c0ed5386e3f0b3005bdb60b
+KEY: &APPID=986bc7f39c0ed5386e3f0b3005bdb60b
+Search: ?q=
+*/
 
-const key = "&APPID=986bc7f39c0ed5386e3f0b3005bdb60b";
-const searchLocation = "Vancouver";
+const noResultErrorCode = 404;
+
+const weatherKey = "&APPID=986bc7f39c0ed5386e3f0b3005bdb60b";
+const searchLocation = "New York";
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const month = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
 
 const getWeatherData = async (location) => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}${key}`);
-  if(response.ok)
-  {
-    const data = await response.json();
-    console.log(data);
-    return data;
+  try{
+    setSearchingText();
+    lockSearchField();
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}${weatherKey}`);
+    if(response.ok)
+    {
+      const data = await response.json();
+      setWeatherData(data);
+      return data;
+    }
+    else if(response.status === noResultErrorCode)
+    {
+      const inputTextField = document.querySelector('#user-input-location');
+      inputTextField.classList.add('error'); 
+      // remove the class after the animation completes
+      setTimeout(function() {
+        inputTextField.classList.remove('error');
+      }, 300);
+      throw "No such city.";
+    }
+    else
+    {
+      throw "Unexpected error!"
+    }
   }
-  else
+  catch(e)
   {
-    console.log('Somethime wrong', response);
+    console.error(e);
+  }
+  finally
+  {
+    unlockSearchField();
   }
 }
 
-console.log("hello world!");
+function setSearchingText()
+{
+  const location = document.querySelector('.location');
+  location.innerText = 'Searching';
+
+  const date = document.querySelector('.date');
+  date.innerText = 'Searching';
+
+  const temperature = document.querySelector('.temperature');
+  temperature.innerText = 'Searching';
+
+  const low_temp = document.querySelector('.low-temp');
+  const high_temp = document.querySelector('.high-temp');
+
+  low_temp.innerText = 'Searching';
+  high_temp.innerText = 'Searching';
+}
+
+function lockSearchField()
+{
+  const searchField = document.querySelector('#user-input-location');
+  searchField.disabled = true;
+}
+
+function unlockSearchField()
+{
+  const searchField = document.querySelector('#user-input-location');
+  searchField.disabled = false;
+}
+
+function setWeatherData(data)
+{
+  console.warn('Setting data');
+  const location = document.querySelector('.location');
+  location.innerText = `${data.name}, ${data.sys.country}`;
+
+  const today = new Date();
+  const date = document.querySelector('.date');
+  date.innerText = `${weekday[today.getDay()]} ${today.getDate()} ${month[today.getMonth()]} ${today.getFullYear()}`;
+
+  const temperature = document.querySelector('.temperature');
+  temperature.innerText = `${getCelciusFromKelvin(data.main.temp)} \u2103`;
+
+  const low_temp = document.querySelector('.low-temp');
+  const high_temp = document.querySelector('.high-temp');
+
+  low_temp.innerText = `${getCelciusFromKelvin(data.main.temp_min)} \u2103`;
+  high_temp.innerText = `${getCelciusFromKelvin(data.main.temp_max)} \u2103`;
+
+}
+
+function getCelciusFromKelvin(K)
+{
+  return Math.round(K - 273.15);
+}
+
+function searchBarHandle(event)
+{
+  if(event.keyCode === 13)
+  {
+    const target = event.target;
+    getWeatherData(target.value);
+  }
+
+}
+
+getWeatherData(searchLocation);
